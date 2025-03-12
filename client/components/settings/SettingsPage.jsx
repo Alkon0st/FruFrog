b1c0adfc45ec48816bd4fe28331b0f3ffb1274d3
SophiaNguyen09 <sophianguyen578@gmail.com>
2025-03-11 22:09:55 -0700

    added style pages for profile and settings

--- client/components/settings/SettingsPage.jsx
+++ client/components/settings/SettingsPage.jsx
@@ -1,5 +1,6 @@
 import {StyleSheet, Text, View} from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
+import styles from './SettingsPage.style';
 
 function SettingsPage() {
     return (
