e90e2c31f8479be95f569aeba6b7b89d1573e797
SophiaNguyen09 <sophianguyen578@gmail.com>
2025-03-15 22:29:46 -0700

    removed the temp create pond code from Home

--- client/components/home/HomePage.jsx
+++ client/components/home/HomePage.jsx
@@ -6,91 +6,22 @@ import { useNavigation } from 'expo-router';
 import { useForm, Controller } from 'react-hook-form';
 import Select from 'react-select';
 
-
 import ProfilePage from '../profile/ProfilePage'
 import SettingsPage from '../settings/SettingsPage'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function CreatePond () {
-    const navigation = useNavigation();
-
-    return (
-        <View style ={styles.viewStyle}>
-            <Text style ={styles.headingStyle}>Create Pond Page</Text>
-            <Text style ={styles.textStyle}>This is the placeholder for the history page</Text>
-            <Button title="Go Back" onPress={() => navigation.goBack()} />
-        </View>
-    );
-}
-
-const Stack = createNativeStackNavigator();
+import DropDown from '../pondFunctions/ponddropdown';
 
 function HomePage() {
     const navigation = useNavigation();
 
-    //for sample options
-    const options = [
-        {value: '1', label: 'option 1'},
-        {value: '2', label: 'option 2'},
-        {value: '3', label: 'option 3'},
-        {value: '4', label: 'option 4'},
-    ]
-
-    //for form
-    const {
-        control, 
-        handleSubmit,
-        formState: {errors},
-    } = useForm({
-        defaultValues: {
-            groupName: "",
-        },
-    })
-
-    //takes input data and puts it in console
-    const onSubmit = (data) => console.log(data)
-
     return (
         <View style ={styles.viewStyle}>
             <Text style ={styles.headingStyle}>Home Page</Text>
-            <Text style ={styles.textStyle}>This is the placeholder for the home page</Text>            
-            <Button title = 'Create Pond(WIP)' onPress={() => navigation.navigate(CreatePond)} />
+            <Text style ={styles.textStyle}>This is the placeholder for the home page</Text>
             
-            
-            <Text style={styles.headingStyle}>Start of Create Pond Form</Text>
-            <Text style ={styles.textStyle}>Enter Pond Name:</Text>
-            <Controller
-                control={control}
-                rules={{
-                required: true,
-                }}
-                render={({ field: { onChange, onBlur, value } }) => (
-                <TextInput
-                    placeholder="Pond Name"
-                    onBlur={onBlur}
-                    onChangeText={onChange}
-                    value={value}
-                />
-                )}
-                name="groupName"
-            />
-            {errors.groupName && <Text>This is required.</Text>}
-            
-            <Text style ={styles.textStyle}>Choose Pond Thumbnail:</Text>
-            <Controller
-                control={control}
-                name="thumbnail"
-                render= {({ field: {onChange, onBlur, value} }) => (
-                    <Select
-                        options={options}
-                        onBlur={onBlur}
-                        onChange={onChange}
-                        value={value}
-                    />
-            )} />
-            {errors.thumbnail && <Text>This is required.</Text>}
-
-            <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+            {/* Function to call for create pond */}
+            <DropDown /> 
         </View>
     );
 }
@@ -115,12 +46,3 @@ const styles = StyleSheet.create({
 });
 
 export default HomePage
-
-
-{/* <Stack.Navigator>
-<Stack.Screen
-name="Create Pond"
-component={CreatePond}
-options={{ headerShown: false}}
-/>
-</Stack.Navigator> */}
