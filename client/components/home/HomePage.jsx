23183b241851dc8a7ef4c294091f249c779b479a
SophiaNguyen09 <sophianguyen578@gmail.com>
2025-03-15 22:47:34 -0700

    cleaned up imports from HomePage.jsx

--- client/components/home/HomePage.jsx
+++ client/components/home/HomePage.jsx
@@ -1,20 +1,8 @@
 import {StyleSheet, Text, View, Button} from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { useNavigation } from 'expo-router';
-import { useForm, Controller } from 'react-hook-form';
-import Select from 'react-select';
-
-import ProfilePage from '../profile/ProfilePage'
-import SettingsPage from '../settings/SettingsPage'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import CreatePond from '../pondFunctions/CreatePond';
 
 function HomePage() {
-    const navigation = useNavigation();
-
     return (
         <View style ={styles.viewStyle}>
             <Text style ={styles.headingStyle}>Home Page</Text>
