5b2baf03f1f8c8446f0389cbdcef2e2c698be809
SophiaNguyen09 <sophia.nguyen09@student.csulb.edu>
2025-04-07 10:41:40 -0700

    moved createpond popups to headernav

--- client/components/nav/HeaderNav.jsx
+++ client/components/nav/HeaderNav.jsx
@@ -3,28 +3,68 @@ import * as React from 'react';
 // all the pages 
 import ProfilePage from '../profile/ProfilePage'
 import SettingsPage from '../settings/SettingsPage'
+import AddUser from '../pondFunctions/AddUser';
+import CreatePage from '../pondFunctions/CreatePondFolder/CreatePage';
 
 import styles from './Nav.style';
 
-import { View, Text, TouchableOpacity } from 'react-native';
+import { Pressable, View, Text, TouchableOpacity, Modal } from 'react-native';
 import { useState } from 'react';
 
 
 export default function HeaderNav() {
-      const [isProfileModalVisible, setIsProfileModalVisible] = useState(false);
+    const [isProfileModalVisible, setIsProfileModalVisible] = useState(false);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [pondName, setPondName] = useState("Current Pond");
 
     return (
-        // Modal call button for header nav
-        <View style={styles.mainView}>
-            <TouchableOpacity onPress={() => setIsProfileModalVisible(true)} style={[styles.button, styles.buttonOpen]}>
-                <Text>Profile</Text>
-            </TouchableOpacity>
+        <View>
+            {/*The popart part*/}
+            <Modal 
+            animationType="none"
+            transparent = {true}
+            visible={modalVisible}
+            onRequestClose={() => {
+                Alert.alert('Modal closed.');
+                setModalVisible(!modalVisible); //changes variable to not visible
+            }}>
+            {/* The popup itself */}
+            <View style={styles.mainView}>
+                <View style={styles.modalView}>
+                <Pressable
+                    style={[styles.button, styles.buttonClose]}
+                    onPress={() => setModalVisible(!modalVisible)}>
+                    <Text style={styles.textButton}>Hide</Text>
+                </Pressable>
+                <View style={styles.buttonRow}>
+                    {/* Function to call for create pond */}
+                    <AddUser />
+                    <CreatePage /> 
+                </View>
+                </View>
+            </View>
+            </Modal>
+            <View>
+                {/*The button to summon popup*/}
+                <Pressable
+                    style={[styles.button, styles.buttonOpen]}
+                    onPress={() => setModalVisible(true)}>
+                    <Text style={styles.textButton}>{pondName} ▼</Text>
+                </Pressable>
+            </View>
+            
+            {/* Modal call button for header nav */}
+            <View style={styles.mainView}>
+                {/* profile button */}
+                <TouchableOpacity onPress={() => setIsProfileModalVisible(true)} style={[styles.button, styles.buttonOpen]}>
+                    <Text>Profile</Text>
+                </TouchableOpacity>
 
-            <ProfilePage
-            visible={isProfileModalVisible}
-            onClose={() => setIsProfileModalVisible(false)} 
-            />
+                <ProfilePage
+                visible={isProfileModalVisible}
+                onClose={() => setIsProfileModalVisible(false)} 
+                />
+            </View>
         </View>
-
     );
 }
--- client/components/nav/Nav.style.jsx
+++ client/components/nav/Nav.style.jsx
@@ -3,13 +3,18 @@ import { StyleSheet, Dimensions } from "react-native";
 const { width, height } = Dimensions.get('window')
 
 const styles = StyleSheet.create({
+    // mainView: {
+    //     display: 'flex',
+    //     justifyContent: 'left',
+    //     alignItems: 'flex-start',
+    //     flex: 1,
+    //     marginLeft: '10px',
+    // },
     mainView: {
-        display: 'flex',
-        justifyContent: 'left',
-        alignItems: 'flex-start',
-        flex: 1,
-        marginLeft: '10px',
-        marginRight: '10px',
+      flex: 1,
+      justifyContent: 'top',
+      alignItems: 'center',
+      backgroundColor: 'rgba(179, 228, 183, 0.5)'
     },
     button: {
         margin: 5,
@@ -24,6 +29,40 @@ const styles = StyleSheet.create({
     },
     buttonClose: {
         backgroundColor: '#2196F3',
+    },
+    buttonRow: {
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignSelf: 'stretch',
+        paddingRight: '20px',
+        paddingLeft: '20px',
+        paddingTop: '10px',
+    },
+    modalView: {
+      backgroundColor: 'white',
+      borderBottomLeftRadius: 20,
+      borderBottomRightRadius: 20,
+      padding: 35,
+      alignItems: 'center',
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5,
+      width: width,
+      alignSelf: 'flex-start',
+    },
+    button: {
+        margin: 5,
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+        backgroundColor: '#89d149',
+        alignItems: 'center',
+        flex: 1,
     },
 });
 
