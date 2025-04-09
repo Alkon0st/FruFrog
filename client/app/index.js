a9b22134fd9a745dfadf1986b47a90b8b03bf995
Alkon0st <151362849+Alkon0st@users.noreply.github.com>
2025-04-09 01:44:57 -0700

    Update index.js

--- client/app/index.js
+++ client/app/index.js
@@ -1,35 +1,20 @@
 import { View, Text, SafeAreaView, TouchableOpacity} from 'react-native';
-import Reac, {useState} from 'react';
-import { Stack, useRouter } from 'expo-router';
-import HookComponent from '../components/hookComponent';
+import { NavigationContainer, NavigationIndependentTree } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import {Budget, Nav, HomePage, HeaderButton} from '../components';
 
-const Home = () => {
-    const router = useRouter();
 
-    const [showHookComponent, setShowHookComponent] = useState(false);
-    const fetchData = () => {
-    setShowHookComponent(true);
-    };
+const Stack = createStackNavigator();
+const Home = () => {
 
     return (
-        <Nav/>
-        // <HomePage/>
-        // <SafeAreaView>
-        //     <View>
-        //         <Text>Home</Text>
-        //         <Nav/>
-        //         <Budget/>
-
-
-        //         {/* this section is for api call testing*/}
-        //         {showHookComponent && <HookComponent />}
-        //         <TouchableOpacity onPress={fetchData} style={{ padding: 10, backgroundColor: 'blue', borderRadius: 5 }}>
-        //             <Text style={{ color: 'white', textAlign: 'center' }}>Fetch Data</Text>
-        //         </TouchableOpacity>
-                
-        //     </View>
-        // </SafeAreaView>
+        <NavigationIndependentTree>
+            <NavigationContainer>
+                <Stack.Navigator name="Home" component={HomePage} />
+            </NavigationContainer>
+        </NavigationIndependentTree>
+    //    <Nav/>
+        
     );
 };
 export default Home;
