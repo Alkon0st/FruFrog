ccc9162a99b5124784ce18755edbcde17d08e2b2
Alkon0st <151362849+Alkon0st@users.noreply.github.com>
2025-04-15 02:39:39 -0700

    shh

--- client/firebase/firebase.js
+++ client/firebase/firebase.js
@@ -8,7 +8,7 @@ import {getAuth} from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBq7tf40JqHnXpjsgtQ3CsW6uXWm-MwQg0",
+  apiKey: GOOGLE_FIREBASE_API,
   authDomain: "frufrog-c042c.firebaseapp.com",
   projectId: "frufrog-c042c",
   storageBucket: "frufrog-c042c.firebasestorage.app",
