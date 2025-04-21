a58a665f7848f9a7a46d4d98d65c35524f352e76
SophiaNguyen09 <sophia.nguyen09@student.csulb.edu>
2025-04-21 10:55:33 -0700

    erased content of profile page

--- client/components/profile/ProfilePage.jsx
+++ client/components/profile/ProfilePage.jsx
@@ -24,58 +24,9 @@ const ProfilePage = ({
             {/* Main portion of profile */}
             <View style ={styles.page}>
                 <View style={styles.profileContainer}>
-                <Text style={styles.profileName}>Profile Name</Text>
                 </View>
 
-                <View style={styles.informationContainer}>
-                    <Text style={styles.informationHeader}>Public Information</Text>
-                </View>
-                {/* Username */}
-                <View style={styles.promptItem}>
-                <Text>Username: </Text>
-                <TextInput style={styles.promptInput} placeholder="Username" />
-                </View>
-                {/* First name */}
-                <View style={styles.promptItem}>
-                <Text>First Name: </Text>
-                <TextInput style={styles.promptInput} placeholder="Username" />
-                </View>
-                {/* Last name */}
-                <View style={styles.promptItem}>
-                <Text>Last Name: </Text>
-                <TextInput style={styles.promptInput} placeholder="Username" />
-                </View>
-                {/* Pronouns */}
-                <View style={styles.promptItem}>
-                <Text>Pronouns: </Text>
-                <TextInput style={styles.promptInput} placeholder="Username" />
-                </View>
-                {/* Occupation */}
-                <View style={styles.promptItem}>
-                <Text>Occupation: </Text>
-                <TextInput style={styles.promptInput} placeholder="Username" />
-                </View>
-                {/* Bio */}
-                <View style={styles.promptItem}>
-                <Text>Bio: </Text>
-                <TextInput style={styles.promptInput} placeholder="Username" />
-                </View>
-
-                <Text style={styles.question}>
-                    Looking for account settings?
-                </Text>
-                    <TouchableOpacity style={styles.questionButton}>
-                        <Text style={styles.questionButtonText}>Redirect to Account Settings</Text>
-                    </TouchableOpacity>
-                <Text style={styles.question}>
-                    Want to make your profile private?
-                </Text>
-                    <TouchableOpacity style={styles.questionButton}>
-                        <Text style={styles.questionButtonText}>Redirect to Account Settings</Text>
-                    </TouchableOpacity>
-                <TouchableOpacity style={styles.logoutButton}>
-                    <Text style={styles.logoutButtonText}>Log Out</Text>
-                </TouchableOpacity>
+                
             </View>
         </Modal>
     );
--- client/components/profile/ProfilePage.style.jsx
+++ client/components/profile/ProfilePage.style.jsx
@@ -8,7 +8,6 @@ const styles = StyleSheet.create({
         color: '#000000',
         fontSize: 30,
     },
-
     modal: {
         height: height,
         width: '100%',
@@ -23,40 +22,8 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
     },
-    informationContainer: {
-        justifyContent: 'left',
-        paddingBottom: 10,
-        borderBottomWidth: 3    ,
-        borderBottomColor: '#000000',
-        width: '75%',
-        marginBottom: 10,
-    },
     profileContainer: {
         paddingBottom: 10,
-    },
-    promptItem: {
-        flexDirection: 'row',
-        alignContent: 'left',
-        justifyContent: 'center',
-        paddingBottom: 10,
-    },
-    promptInput: {
-        backgroundColor: '#b2e196',
-    },
-    questionButton: {
-        backgroundColor: '#85bb65',
-        borderColor: '#4f723a',
-        borderWidth: 2,
-        borderRadius: 8,
-        padding: 3,
-        marginBottom: 10,
-    },
-    logoutButton: {
-        backgroundColor: '#ff6060',
-        borderColor: '#590000',
-        borderWidth: 2,
-        borderRadius: 8,
-        padding: 3,
     },
     // Text styles
     pageHeader: {
@@ -65,24 +32,6 @@ const styles = StyleSheet.create({
         color: '#557549',
         textAlign: 'center',
     },
-    profileName: {
-        color: '#000000',
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
-    informationHeader: {
-        fontWeight: 'bold',
-    },
-    prompt: {
-    },
-    question: {
-    },
-    questionButtonText: {
-        color: '#ffffff',
-    },
-    logoutButtonText: {
-        fontWeight: 510,
-    }
 });
 
 export default styles;
