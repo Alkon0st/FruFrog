411380cdfb603b0eaac976ee1b2a749e8058c778
SophiaNguyen09 <sophianguyen578@gmail.com>
2025-04-21 23:20:25 -0700

    start of profile page

--- client/components/profile/ProfilePage.style.jsx
+++ client/components/profile/ProfilePage.style.jsx
@@ -1,37 +1,108 @@
 import { StyleSheet, Dimensions } from "react-native";
 
-const { width, height } = Dimensions.get('window')
-
 const styles = StyleSheet.create({
+    page: { 
+        flex: 1,
+    },
+    bottomContainer: {
+        
+    },
+
+    //Header 
+    headerContainer: {
+        margin: 10,
+        paddingTop: 40,
+        position: 'relative',
+        paddingHorizontal: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    header: {
+        flex: 1,
+    },
+    headingStyle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#22470C',
+        textAlign: 'center',
+    },
     buttonText: {
         fontWeight: 'bold',
         color: '#000000',
         fontSize: 30,
     },
-    modal: {
-        height: height,
-        width: '100%',
-    },
-    page: {
-        justifyContent: 'center',
+
+    // Content
+    contentContainer: {
+        backgroundColor: '#B2E196',
+        borderRadius: 6,
         alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'center',
+        marginVertical: 20,
+        width: 332,
+        minHeight: 128,
     },
-    // View styles
-    headerContainer: {
-        margin: '10px',
+
+    //Confirm Changes button
+    confirmButton: {
+        margin: 5,
+        marginBottom: 20,
+        padding: 10,
+        minWidth: 174,
+        height: 40,
+        borderWidth: 2,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignSelf: 'center',
+        alignContent: 'center',
+        backgroundColor: '#85BB65',
+        borderColor: '#4F723A',
+    },
+    confirmText: {
+        color: '#FFFFFF',
+        fontSize: 15,
+        textAlign: 'center', 
+    },
+
+    //Logout
+    logOutButton: {
+        backgroundColor: '#FF6060',
+        borderRadius: 5,
+        borderWidth: 2,
+        borderColor: '#590000',
+        width: 174,
+        height: 30,
+        alignSelf: 'center',
+        justifyContent: 'center',
+        alignContent: 'center',
+        padding: 18,
+    },
+    logOutContent: {
         flexDirection: 'row',
-        justifyContent: 'space-between',
+        justifyContent: 'center',
     },
-    profileContainer: {
-        paddingBottom: 10,
+    logOutImg: {
+        width: 20,
+        height: 20,
+        tintColor: '#000000',
+        marginRight: 5,
     },
-    // Text styles
-    pageHeader: {
-        fontSize: 24,
+    logOutText: {
         fontWeight: 'bold',
-        color: '#557549',
+        fontSize: 16,
+        color: '#000000',
         textAlign: 'center',
     },
+
+    
+  footer: {
+    fontSize: 12,
+    color: '#888',
+    marginTop: 10,
+    marginBottom: 10,
+    textAlign: 'center',
+    },  
 });
 
 export default styles;
