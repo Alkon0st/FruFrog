da752bf56069342d364511c8c7e3fb4873e8cb10
SophiaNguyen09 <sophianguyen578@gmail.com>
2025-04-22 22:01:48 -0700

    added thumbnail carousel to edit thumbnail

--- client/components/pondFunctions/editPond.jsx
+++ client/components/pondFunctions/editPond.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { View, Text, TextInput, ScrollView, TouchableOpacity, Image } from 'react-native';
 
 
 import styles from '../settings/SettingsPage.style';
@@ -15,14 +15,24 @@ export default function EditPond ({
     visible }) {
     const [ newPondName, setNewPondName ] = useState(pondName);
     const [ newThumbnail, setNewThumbnail ] = useState(thumbnail);
+
+    //for thumbnail options
+    let thumbnailOptions = [1,2,3,4,5,6,7,8]
     
-    const handleRenamePond = () => {
-        if (newPondName && newPondName !== pondName) {
-            renamePond(pondName, newPondName);
-            setPondName(newPondName);
+    const handleEditPond = () => {
+        if ((newPondName && newPondName !== pondName) || newThumbnail !== thumbnail) {
+
+            if (newPondName && newPondName !== pondName) {
+                setPondName(newPondName);
+            }
+            
+            if (newThumbnail !== thumbnail) {
+                setThumbnail(newThumbnail);
+            }
+
             triggerUpdate()
         }
-    }
+    };
 
     if (!visible) return null;
 
@@ -31,7 +41,29 @@ export default function EditPond ({
             {/* Change Thumbnail */}
             <Text style={styles.dropDownHeader}>Pond Thumbnail</Text>
             <View style={styles.dropDownItem}>
-                
+                <ScrollView 
+                    horizontal 
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={styles.editThumbnailScrollContent}
+                >
+                    {thumbnailOptions.map((value) => {
+                        const isSelected = newThumbnail === value;
+                        return(
+                            <TouchableOpacity
+                            key={value}
+                            style={styles.editThumbnailButton}
+                            onPress={() => setNewThumbnail(value)}
+                            >
+                                <View style={{ position: 'relative'}}>
+                                    <PondThumbnail selection={value} optionalStyle={{width: 60, height: 60}}/>
+                                    {isSelected && (
+                                        <View style = {styles.selectedOverlay}/>
+                                    )}
+                                </View>
+                            </TouchableOpacity>
+                        )}
+                    )}
+                </ScrollView>
             </View>
 
             {/* Change Pond Name */}
@@ -50,10 +82,10 @@ export default function EditPond ({
 
             {/* Confirm Changes */}
             <TouchableOpacity 
-            onPress={handleRenamePond}
+            onPress={handleEditPond}
             style={styles.confirmButton}> 
                 <Text style={styles.confirmText}> Confirm All Changes </Text> 
             </TouchableOpacity>
         </View>    
-    )
-};
+    );
+}
