f2839dfd127aa1859719302441b41a19c6a7c0cc
SophiaNguyen09 <sophianguyen578@gmail.com>
2025-04-22 20:00:57 -0700

    added profile presets

--- client/components/profile/img/profilePicture.jsx
+++ client/components/profile/img/profilePicture.jsx
@@ -3,6 +3,21 @@ import { Image, StyleSheet, View } from "react-native";
 // list of thumbnail images, each mapped to a number
 const images = {
     1: require('./1.png'),
+    2: require('./2.png'),
+    3: require('./3.png'),
+    4: require('./4.png'),
+    5: require('./5.png'),
+    6: require('./6.png'),
+    7: require('./7.png'),
+    8: require('./8.png'),
+    9: require('./9.png'),
+    10: require('./10.png'),
+    11: require('./11.png'),
+    12: require('./12.png'),
+    13: require('./13.png'),
+    14: require('./14.png'),
+    15: require('./15.png'),
+    16: require('./16.png'),
 }
 
 
